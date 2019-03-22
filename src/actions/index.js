8bb93c5503f26ce0589b0bee66b8b7eac1263c2c
shetty4l <suyashshetty29@gmail.com>
2019-03-22 16:07:54 -0700

    Action for fetching comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,18 @@
-import { SAVE_COMMENT } from 'actions/types'
+import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types'
 
 export function saveComment(comment) {
   return {
     type: SAVE_COMMENT,
     payload: comment
-  };
+  }
+}
+
+export async function fetchComments() {
+  const url = 'https://jsonplaceholder.typicode.com/comments';
+  const response = await fetch(url);
+
+  return {
+    type: FETCH_COMMENTS,
+    payload: response.json()
+  }
 }
